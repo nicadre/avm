6dc352451e49e876a6bef73f43d73bad26a90ec8
Leo LAPILLONNE <llapillo@student.42.fr>
2016-01-11 18:56:40 +0100

    proprify ioperand

--- includes/IOperand.interface.hpp
+++ includes/IOperand.interface.hpp
@@ -6,12 +6,12 @@
 //   By: llapillo <llapillo@student.42.fr>          +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2016/01/11 14:12:54 by llapillo          #+#    #+#             //
-//   Updated: 2016/01/11 15:43:09 by llapillo         ###   ########.fr       //
+//   Updated: 2016/01/11 18:54:44 by llapillo         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
-#ifndef IOPERAND_INTERFACE_HPP
-# define IOPERAND_INTERFACE_HPP
+#ifndef		IOPERAND_INTERFACE_HPP
+# define	IOPERAND_INTERFACE_HPP
 
 # include <iostream>
 
@@ -29,14 +29,17 @@ public:
 
 	virtual	int							getPrecision(void) const = 0;
 	virtual	eOperandType				getType(void) const = 0;
+
+	virtual	std::string		const	&	toString(void) const = 0;
+
 	virtual	IOperand		const	*	operator+(IOperand const & rhs) const = 0;
 	virtual	IOperand		const	*	operator-(IOperand const & rhs) const = 0;
 	virtual	IOperand		const	*	operator*(IOperand const & rhs) const = 0;
 	virtual	IOperand		const	*	operator/(IOperand const & rhs) const = 0;
 	virtual	IOperand		const	*	operator%(IOperand const & rhs) const = 0;
-	virtual	std::string		const	&	toString(void) const = 0;
+
 	virtual	~IOperand(void) {}
 
 };
 
-#endif
+#endif	//	IOPERAND_INTERFACE_HPP
